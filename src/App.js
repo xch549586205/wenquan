0a3a92e0288fe865eb9436fd23c35087c40d9502
xiechenhua <chenhua.xie@broadlink.com.cn>
2022-06-02 17:39:47 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import routers from "./router";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default class extends React.PureComponent {
+  render() {
+    return (
+      <Routes
+        location={this.props.location}
+        history={this.props.history}
+        level={1}
+      >
+        {routers.map((router) => {
+          const { Component, path } = router;
+          return <Route key={path} path={path} element={<Component />} />;
+        })}
+      </Routes>
+    );
+  }
 }
-
-export default App;
