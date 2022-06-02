11e2b0cef492786552a521b37e9ab94e2ce543de
xiechenhua <chenhua.xie@broadlink.com.cn>
2022-06-02 19:01:28 +0800

    1

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,31 @@
 import React from "react";
-import { Button } from "antd";
+import { Row, Col } from "antd";
+import { Tabs } from "antd";
+
+const { TabPane } = Tabs;
+
 class Main extends React.PureComponent {
+  onChange = (key) => {
+    console.log(key);
+  };
   render() {
     return (
-      <div>
-        <Button type="primary">Button</Button>
-      </div>
+      <Row>
+        <Col span={3}>
+          <Tabs defaultActiveKey="1" onChange={this.onChange}>
+            <TabPane tab="题型" key="1">
+              题型
+            </TabPane>
+            <TabPane tab="题库" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="大纲" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+        </Col>
+        <Col span={21}>col-6 col-pull-18</Col>
+      </Row>
     );
   }
 }
